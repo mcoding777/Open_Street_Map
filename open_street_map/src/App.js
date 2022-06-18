9b9f91b4516f5671a64ecbc54ffc7e64b239f0f0
misun <mcoding777@naver.com>
2022-06-18 21:00:02 +0900

    feat: OSM과 React-Leaflet으로 오프라인 지도 구현

--- open_street_map/src/App.js
+++ open_street_map/src/App.js
@@ -1,7 +1,42 @@
-import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 function App() {
-  return <div className="App">지도</div>;
+  const position = [36.9737541905668, 127.932764159959];
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={16}
+      scrollWheelZoom={true}
+      style={{ width: "100vw", height: "100vh" }}
+      minZoom={16}
+      maxZoom={17}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="./images/Tiles/{z}/{x}/{y}.png"
+      />
+      <Marker
+        icon={L.icon({
+          iconUrl: markerIcon,
+          iconRetinaUrl: markerIcon2x,
+          shadowUrl: markerShadow,
+          iconAnchor: [15, 41],
+          iconSize: [30, 40],
+        })}
+        position={position}
+      >
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
 export default App;
